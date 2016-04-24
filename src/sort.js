e22e73d5b532bbacae200a10b42e565cc4505ced
avin45h <avinash.verma.hbk@gmail.com>
2016-04-24 22:37:12 +0530

    Update: added bubble sort

--- src/sort.js
+++ src/sort.js
@@ -14,15 +14,24 @@ var sort = exports = module.exports = {
         }
         return arr;
     },
-    bubble: function(){
-        for(let i = 0; i < arr.length; i++ ){
-
+    bubble: function (arr) {
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < (arr.length - 1) - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    util.swap(arr, j, j + 1);
+                }
+            }
         }
+        return arr;
     }
 };
 
 var util = require('./util');
-util.analyze(function (init, done) {
+util.anal   yze(function (init, done) {
     console.log(sort.selection([8, 7, 6, 2, 4, 1, 3, 5, 1, 3]));
+    done(init);
+});
+util.analyze(function (init, done) {
+    console.log(sort.bubble([8, 7, 6, 2, 4, 1, 3, 5, 1, 3]));
     done(init);
 });
